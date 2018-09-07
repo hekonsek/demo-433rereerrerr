e3f10003e00ad1711ea914a6bf7e2397c8ebe2e5
Henryk Konsek <hekonsek@gmail.com>
2018-09-07 11:41:03 +0200

    Initial import

---
+++ src/test/java/com/example/demo433rereerrerr/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo433rereerrerr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
